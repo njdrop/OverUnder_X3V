98aba3d33849592639c589e7e248b13e4758674f
Noah Dropsey <njdropsey@gmail.com>
2024-01-10 16:59:56 -0500

    Revert "Revert "fixed maybe fr""
    This reverts commit 1c8a62c6674d0c7624110ab4f8257a144b79d021.

--- src/configuration/robotConfig.cpp
+++ src/configuration/robotConfig.cpp
@@ -7,30 +7,15 @@ using namespace vex;
 vex::brain Brain;
 vex::controller con;
 // motors
-<<<<<<< HEAD
-vex::motor leftMotor1 (PORT5, ratio6_1, true);//1
-vex::motor leftMotor2 (PORT8, ratio6_1, true);//2
-vex::motor leftMotor3 (PORT15, ratio6_1, true);//3
-vex::motor rightMotor1 (PORT1, ratio6_1, false);//8
-vex::motor rightMotor2 (PORT2, ratio6_1, false);//9
-vex::motor rightMotor3 (PORT20, ratio6_1, false);//10
-vex::motor intake (PORT11, ratio18_1, false);//16
-vex::motor kicker1 (PORT21, ratio18_1, true);//18
-vex::motor kicker2 (PORT19, ratio18_1, true);//19
-=======
 vex::motor leftMotor1 (PORT1, ratio6_1, true);
 vex::motor leftMotor2 (PORT2, ratio6_1, true);
 vex::motor leftMotor3 (PORT3, ratio6_1, true);
 vex::motor rightMotor1 (PORT8, ratio6_1, false);
 vex::motor rightMotor2 (PORT9, ratio6_1, false);
 vex::motor rightMotor3 (PORT10, ratio6_1, false);
-vex::motor slapper1 (PORT18, ratio18_1, true);
-vex::motor slapper2 (PORT19, ratio18_1, true);
 vex::motor intake (PORT16, ratio18_1, false);
 vex::motor kicker1 (PORT18, ratio18_1, true);
 vex::motor kicker2 (PORT19, ratio18_1, true);
-vex::motor intake (PORT21, ratio6_1, true);
->>>>>>> parent of d7dc326 (fixed error and merged branches)
 // pneumatics
 vex::pneumatics intakeLift (Brain.ThreeWirePort.A);
 vex::pneumatics lift (Brain.ThreeWirePort.B);
